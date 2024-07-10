e2b620d1641e33bbca98c5387ca4d34a165becc3
Matheus Mota <matheuzwork@gmail.com>
2024-07-10 16:31:29 -0300

    inverter

--- display.c
+++ display.c
@@ -13,15 +13,15 @@
 
 int numbers(char c) {
     switch (c) {
-        case '1': return 0b00000110;
-        case '2': return 0b01011011;
-        case '3': return 0b01001111;
-        case '4': return 0b01100110;
-        case '5': return 0b01101101;
-        case '6': return 0b01111101;
-        case '7': return 0b00000111;
-        case '8': return 0b01111111;
-        case '9': return 0b01101111;
+        case '1': return 0b11111001;
+        case '2': return 0b10100100;
+        case '3': return 0b10110000;
+        case '4': return 0b10011001;
+        case '5': return 0b10010010;
+        case '6': return 0b10000010;
+        case '7': return 0b11111000;
+        case '8': return 0b10000000;
+        case '9': return 0b10010000;
     }
 }
 int main(void) {
